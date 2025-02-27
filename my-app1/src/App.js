1dc8c8c3ad03f781a7cbe76bda7d8b552a994b07
madhav <uniquemadhav@gmail.com>
2025-02-27 13:00:24 +0530

    Day 1

---
+++ my-app1/src/App.js
@@ -0,0 +1,21 @@
+// const App1 = ()=>{
+
+//     return <h2>Hello Shasha welcome to React World.</h2>
+
+// }
+// const App2 = ()=>{
+
+//     return <h2>Hello Shasha welcome to React World.</h2>
+
+// }
+// const App3 = ()=>{
+
+//     return <h2>Hello Shasha welcome to React World.</h2>
+
+// }
+
+
+export const App1 = ()=> <h1>Hello Shasha</h1> ;
+export const App2 = ()=> <h2>Welcome To React world.</h2>;
+export const App3 = ()=> <h3>Come on Shasha you can do it...</h3> ;
+
